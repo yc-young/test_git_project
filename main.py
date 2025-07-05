9062c3da71b8f37ce45b19b3be4089709c795ad5
mac <1011219316@qq.com>
2025-07-05 12:32:32 +0800

    更新1

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('feature2')
-    print('123')
+    print('修改')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
